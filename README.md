da1524fb15c98520690d5cca9071e663cb9e433a
Josef Gabrielsson <josef.gabrielsson@gu.se>
2022-02-05 16:16:08 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# BASH USAGE MESSAGE
+An annotation-based usage message that displays an automated command-line interface.
+
+## Output
+
+<pre>
+Usage: ./bash.sh [command] ..
+
+hello_world    Print the message "hello, world".
+random_int     Generate a random int between 0-10.
+help           This help.
+</pre>
---
+++ bash.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+##
+#@ Print the message "hello, world".
+hello_world () {
+  echo "hello, world"
+}
+
+##
+#@ Generate a random int between 0-10.
+random_int () {
+  echo $(( $RANDOM % 10 ))
+}
+
+## Displays the automated command-line interface.
+#@ This help.
+help () {
+  echo -e "Usage: "$0" [command] ..\n"
+  grep -B1 -E "^[%a-zA-Z0-9_-]*[^\s]\(*[^\s])*[^\s]{" "$0" \
+  | grep -v -- -- \
+  | sed 's/()//g' \
+  | sed 'N;s/\n/###/' \
+  | sed -n 's/^#@ \(.*\)###\(.*\){.*/\2###\1/p' \
+  | column -t -s '###'
+  echo ""
+}
+
+# Runs help function if no argument passed.
+if [ -z "$1" ]; then
+  help
+fi
+
+"$@"
